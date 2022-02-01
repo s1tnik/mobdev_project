b30cdcf9906de4f17a07643054875c24ce6f341c
Slava Sitnik <slava2261812@gmail.com>
2022-02-01 18:33:01 +0300

    change input width

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import 'react-native-gesture-handler'
-import React, {useEffect, useState} from "react";
+import "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
 import Dictionary from "./pages/Dicitionary";
 import FlashCards from "./pages/FlashCards";
-import {NavigationContainer} from "@react-navigation/native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import {TextInput} from "react-native";
-import {TailwindProvider} from "tailwind-rn";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { TextInput } from "react-native";
+import { TailwindProvider } from "tailwind-rn";
 import utilities from "./tailwind.json";
-import {useTailwind} from "tailwind-rn/dist";
-
+import { useTailwind } from "tailwind-rn/dist";
 
 export default function App() {
   const [term, setTerm] = useState("");
@@ -24,42 +23,44 @@ export default function App() {
     return () => clearTimeout(timerId);
   }, [term]);
 
-  const handleOnChange = value => {
+  const handleOnChange = (value) => {
     setTerm(value);
   };
 
   const Drawer = createDrawerNavigator();
 
   return (
-      <TailwindProvider utilities={utilities}>
-        <NavigationContainer>
-          <Drawer.Navigator initialRouteName="Dictionary">
-            <Drawer.Screen
-                options={{
-                  headerTitle: () => (
-                      <TextInput
-                          placeholder="Search"
-                          style={[
-                            {
-                              height: 35,
-                              borderWidth: 1,
-                              padding: 10,
-                              borderColor: "#e5e7eb",
-                            },
-                          ]}
-                          onChangeText={handleOnChange}
-                          value={term}
-                      />
-                  ),
-                }}
-                name="Dictionary">
-              {props => (
-                  <Dictionary {...props} term={debouncedTerm} setTerm={setTerm} />
-              )}
-            </Drawer.Screen>
-            <Drawer.Screen name="Flashcards" component={FlashCards} />
-          </Drawer.Navigator>
-        </NavigationContainer>
-      </TailwindProvider>
+    <TailwindProvider utilities={utilities}>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Dictionary">
+          <Drawer.Screen
+            options={{
+              headerTitle: () => (
+                <TextInput
+                  placeholder="Search"
+                  style={[
+                    {
+                      width: 200,
+                      height: 35,
+                      borderWidth: 1,
+                      padding: 10,
+                      borderColor: "#e5e7eb",
+                    },
+                  ]}
+                  onChangeText={handleOnChange}
+                  value={term}
+                />
+              ),
+            }}
+            name="Dictionary"
+          >
+            {(props) => (
+              <Dictionary {...props} term={debouncedTerm} setTerm={setTerm} />
+            )}
+          </Drawer.Screen>
+          <Drawer.Screen name="Flashcards" component={FlashCards} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </TailwindProvider>
   );
 }
